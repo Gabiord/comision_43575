0a310b612ead00cba83d64919786a06560dd426a
HoracioGutierrez <horacio.estevez@gmail.com>
2022-12-13 21:05:25 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hORACIO gUTIERREZ
+          hORACIO gUTIERREz
         </p>
         <a
           className="App-link"
